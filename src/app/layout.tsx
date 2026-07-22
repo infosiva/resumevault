263266f5ecacf64849b6868b3ba17351f7872752
Siva <info.siva@gmail.com>
2026-07-22 23:25:37 +0100

    fix(resumevault): remove dead mixed-content tracker script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -146,7 +146,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       {!isWidgetHidden(theme, 'cookieConsent') && <CookieConsent />}
       {!isWidgetHidden(theme, 'stickyFooterCTA') && <StickyFooterCTA />}
       <Script defer data-domain="resumevault.app" src="https://plausible.io/js/script.js" strategy="afterInteractive" />
-      <Script defer data-site="resumevault.app" src="http://31.97.56.148:3098/t.js" strategy="afterInteractive" />
       <FeedbackWidget siteName="ResumeVault" accentColor="#0ea5e9" position="left" />
       </body>
     </html>
